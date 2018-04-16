1065914252716db4115cb8a7c1f712353b8a6a64
Tatu Pesonen <tatupe360@hotmail.com>
2018-04-16 22:00:52 +0300

    Initial commit

---
+++ Card.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace CCChecker
+{
+    public class CreditCard
+    {
+        public long CardNumber;
+        public string IssuingNetwork;
+        public CreditCard(long CardNumber, string IssuingNetwork)
+        {
+            this.CardNumber = CardNumber;
+            this.IssuingNetwork = IssuingNetwork;
+        }
+    
+        public bool Validate()
+        {
+            string ccstring = CardNumber.ToString();
+            char[] chars = ccstring.ToCharArray();
+            if(chars.Length <= 19)
+            {
+                int sum = 0;
+                for(int i= 0; i < chars.Length; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        int value = Int32.Parse(chars[i].ToString());
+                        value = value * 2;
+                        if (value > 9)
+                        {
+                            sum += (1 + (value % 10));
+                        }
+                        else
+                        {
+                            sum += value;
+                        }
+                    }
+                    else
+                    {
+                        sum += Int32.Parse(chars[i].ToString());
+                    }
+                }
+                if (sum % 10 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;    
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CCChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string json = File.ReadAllText(@"C:\cards.json");
+
+
+            List<CreditCard> CardList = JsonConvert.DeserializeObject<List<CreditCard>>(json);
+            Console.WriteLine(CardList[0].CardNumber);
+
+            foreach (CreditCard cc in CardList)
+            {
+                if (cc.Validate())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("CC number " + cc.CardNumber + " is valid.   | "  + cc.IssuingNetwork);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("CC number " + cc.CardNumber + " is INVALID. | " + cc.IssuingNetwork);
+                }
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
